be7565dd7d748ad8cd42cfc516dfa2b3049225ba
Timofei Zakharov <timchem1508@gmail.com>
2026-08-11 19:30:01 +0200

    Add Wigner-Seitz module and update build configurations

---
+++ src/mctc/wignerseitz/type.f90
@@ -0,0 +1,217 @@
+! This file is part of mctc-lib.
+!
+! Licensed under the Apache License, Version 2.0 (the "License");
+! you may not use this file except in compliance with the License.
+! You may obtain a copy of the License at
+!
+!     http://www.apache.org/licenses/LICENSE-2.0
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
+module wignerseitz_type
+   use mctc_env, only: wp
+   use mctc_io, only: structure_type
+   use mctc_cutoff, only: get_lattice_points
+   implicit none
+   private
+
+   public :: new_wignerseitz_cell, get_wignerseitz_weights, wignerseitz_cell
+
+   type :: wignerseitz_cell
+      integer :: nimg_max
+      integer, allocatable :: nimg(:, :)
+      integer, allocatable :: tridx(:, :, :)
+      real(wp), allocatable :: trans(:, :)
+   end type wignerseitz_cell
+
+
+   !> Small cutoff threshold to create only closest cells
+   real(wp), parameter :: thr = sqrt(epsilon(0.0_wp))
+
+   !> Squared-distance interval for smoothly averaging competing nearest images
+   real(wp), parameter :: tol = 0.3_wp
+
+
+contains
+
+
+   subroutine new_wignerseitz_cell(self, mol)
+
+      !> Wigner-Seitz cell instance
+      type(wignerseitz_cell), intent(out) :: self
+
+      !> Molecular structure data
+      type(structure_type), intent(in) :: mol
+
+      integer :: iat, jat, ntr, nimg
+      integer, allocatable :: tridx(:)
+      real(wp) :: vec(3)
+      real(wp), allocatable :: trans(:, :)
+
+      call get_lattice_points(mol%periodic, mol%lattice, thr, trans)
+      ntr = size(trans, 2)
+      allocate(self%nimg(mol%nat, mol%nat), self%tridx(ntr, mol%nat, mol%nat), &
+      & tridx(ntr))
+
+      !$omp parallel do default(none) schedule(runtime) collapse(2) &
+      !$omp shared(mol, trans, self) private(iat, jat, vec, nimg, tridx)
+      do iat = 1, mol%nat
+         do jat = 1, mol%nat
+            vec(:) = mol%xyz(:, iat) - mol%xyz(:, jat)
+            call get_pairs(nimg, trans, vec, tridx)
+            self%nimg(jat, iat) = nimg
+            self%tridx(:, jat, iat) = tridx
+         end do
+      end do
+
+      call move_alloc(trans, self%trans)
+
+   end subroutine new_wignerseitz_cell
+
+
+   subroutine get_pairs(iws, trans, rij, list)
+      integer, intent(out) :: iws
+      real(wp), intent(in) :: rij(3)
+      real(wp), intent(in) :: trans(:, :)
+      integer, intent(out) :: list(:)
+
+      logical :: mask(size(list))
+      real(wp) :: dist(size(list)), vec(3), r2
+      integer :: itr, img, pos
+      integer :: index(size(list))
+
+      iws = 0
+      img = 0
+      list(:) = 0
+      index(:) = 0
+      mask(:) = .true.
+
+      do itr = 1, size(trans, 2)
+         vec(:) = rij - trans(:, itr)
+         r2 = vec(1)**2 + vec(2)**2 + vec(3)**2
+         if (r2 < thr) cycle
+         img = img + 1
+         dist(img) = r2
+         index(img) = itr
+      end do
+
+      if (img == 0) return
+
+      pos = minloc(dist(:img), dim=1)
+
+      r2 = dist(pos)
+      mask(pos) = .false.
+
+      iws = 1
+      list(iws) = index(pos)
+      if (img <= iws) return
+
+      do
+         pos = minloc(dist(:img), dim=1, mask=mask(:img))
+         if (dist(pos) - r2 > tol) exit
+         mask(pos) = .false.
+         iws = iws + 1
+         list(iws) = index(pos)
+      end do
+
+   end subroutine get_pairs
+
+
+!> Compact C2 switching function for competing nearest images
+   pure elemental function smooth_image_weight(delta) result(weight)
+      real(wp), intent(in) :: delta
+      real(wp) :: weight, x
+
+      x = min(1.0_wp, max(0.0_wp, delta)/tol)
+      weight = max(0.0_wp, 1.0_wp - 10.0_wp*x**3 + 15.0_wp*x**4 - 6.0_wp*x**5)
+   end function smooth_image_weight
+
+
+!> Derivative of the compact switching function with respect to squared distance
+   pure elemental function smooth_image_weight_derivative(delta) result(derivative)
+      real(wp), intent(in) :: delta
+      real(wp) :: derivative, x
+
+      x = min(1.0_wp, max(0.0_wp, delta)/tol)
+      derivative = -30.0_wp*x*x*(1.0_wp - x)**2/tol
+   end function smooth_image_weight_derivative
+
+
+!> Evaluate smooth weights for competing nearest Wigner-Seitz images
+   subroutine get_wignerseitz_weights(self, jat, iat, rij, weight, dwdr, dwdL)
+      !> Wigner-Seitz cell instance
+      type(wignerseitz_cell), intent(in) :: self
+      !> Pair indices in the Wigner-Seitz image arrays
+      integer, intent(in) :: jat, iat
+      !> Cartesian pair vector
+      real(wp), intent(in) :: rij(3)
+      !> Normalized image weights
+      real(wp), intent(out) :: weight(:)
+      !> Weight derivatives with respect to the pair vector
+      real(wp), intent(out), optional :: dwdr(:, :)
+      !> Weight derivatives with respect to strain
+      real(wp), intent(out), optional :: dwdL(:, :, :)
+
+      integer :: img, idx, nimg, refidx
+      real(wp) :: delta, dshape, sum_shape
+      real(wp) :: vec(3), refvec(3), ddelta(3), sum_dr(3)
+      real(wp) :: ddeltaL(3, 3), sum_dL(3, 3)
+
+      weight(:) = 0.0_wp
+      if (present(dwdr)) dwdr(:, :) = 0.0_wp
+      if (present(dwdL)) dwdL(:, :, :) = 0.0_wp
+
+      nimg = self%nimg(jat, iat)
+      if (nimg == 0) return
+
+      refidx = self%tridx(1, jat, iat)
+      refvec = rij - self%trans(:, refidx)
+      sum_shape = 0.0_wp
+
+      do img = 1, nimg
+         idx = self%tridx(img, jat, iat)
+         vec = rij - self%trans(:, idx)
+         delta = max(0.0_wp, dot_product(vec, vec) - dot_product(refvec, refvec))
+         weight(img) = smooth_image_weight(delta)
+         sum_shape = sum_shape + weight(img)
+      end do
+
+      weight(:nimg) = weight(:nimg) / sum_shape
+      if (.not.present(dwdr) .and. .not.present(dwdL)) return
+
+      sum_dr(:) = 0.0_wp
+      sum_dL(:, :) = 0.0_wp
+      do img = 1, nimg
+         idx = self%tridx(img, jat, iat)
+         vec = rij - self%trans(:, idx)
+         delta = max(0.0_wp, dot_product(vec, vec) - dot_product(refvec, refvec))
+         dshape = smooth_image_weight_derivative(delta)
+         ddelta(:) = 2.0_wp*(vec - refvec)
+         ddeltaL(:, :) = 2.0_wp*(spread(vec, 1, 3)*spread(vec, 2, 3) &
+         & - spread(refvec, 1, 3)*spread(refvec, 2, 3))
+         sum_dr(:) = sum_dr + dshape*ddelta
+         sum_dL(:, :) = sum_dL + dshape*ddeltaL
+      end do
+
+      do img = 1, nimg
+         idx = self%tridx(img, jat, iat)
+         vec = rij - self%trans(:, idx)
+         delta = max(0.0_wp, dot_product(vec, vec) - dot_product(refvec, refvec))
+         dshape = smooth_image_weight_derivative(delta)
+         ddelta(:) = 2.0_wp*(vec - refvec)
+         ddeltaL(:, :) = 2.0_wp*(spread(vec, 1, 3)*spread(vec, 2, 3) &
+         & - spread(refvec, 1, 3)*spread(refvec, 2, 3))
+         if (present(dwdr)) then
+            dwdr(:, img) = (dshape*ddelta - weight(img)*sum_dr) / sum_shape
+         end if
+         if (present(dwdL)) then
+            dwdL(:, :, img) = (dshape*ddeltaL - weight(img)*sum_dL) / sum_shape
+         end if
+      end do
+
+   end subroutine get_wignerseitz_weights
+
+end module wignerseitz_type
